de85da8424a339dfa85f2d0dbc5f490b460c66be
Corentin <corentinys@gmail.com>
2026-03-23 14:56:19 +0100

    mise en place de la request api

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+        fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,apparent_temperature,wind_speed_10m,precipitation,relative_humidity_2m&hourly=temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m,precipitation,precipitation_probability")
+            .then(response => response.json())
+            .then(json => setData(json));
+    }, []);
+console.log(data);
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+        <section>
+            <header>
+                <h1>How's the sky looking today</h1>
+                <input type="search" />
+                <button>Search</button>
+            </header>
+            <main>
+                <section>
+                    <div>
+                        <h2>Berlin,germany</h2>
+                        {data.current ? (
+                            <span>{data.current.temperature_2m}°C</span>
+                        ) : (
+                            <span>Loading...</span>
+                        )}
+                    </div>
+                    <div>
+                       <div>
+                           <h2>feel like</h2>
+                           <span>
+        {data.current ? `${data.current.apparent_temperature}°C` : "Loading..."}
+    </span>
+                       </div>
+                        <div>
+                            <h2>humidity</h2>
+                            <span>
+        {data.current ? `${data.current.relative_humidity_2m}%` : "Loading..."}
+    </span>
+                        </div>
+                        <div>
+                            <h2>Wind</h2>
+                            <span>
+        {data.current ? `${data.current.wind_speed_10m} km/h` : "Loading..."}
+    </span>
+                        </div>
+                        <div>
+                            <h2>Precipitation</h2>
+                            {data.current ? (
+                                <span>{data.current.precipitation}</span>
+                            ) : (
+                                <span>Loading...</span>
+                            )}
+                        </div>
+                    </div>
+                </section>
+                <h2>Daily forecast</h2>
+                <section>
+                    <div>
+                        <h2>Tue</h2>
+                        <div>
+                            <span>20 </span>
+                            <span>14 </span>
+                        </div>
 
-      <div className="ticks"></div>
+                    </div>
+                    <div>
+                        <h2>Wed</h2>
+                        <div>
+                            <span>20 </span>
+                            <span>14 </span>
+                        </div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+                    </div>
+                    <div>
+                        <h2>Tuu</h2>
+                        <div>
+                            <span>20 </span>
+                            <span>14 </span>
+                        </div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+                    </div>
+                </section>
+                <aside>
+                    <div>
+                        <h2> Hourly forecast</h2>
+                        <label htmlFor="day">Jour :</label>
+                        <input type="text" id="day" list="day-list" placeholder="Tapez un jours..."/>
+
+                        <datalist id="day-list">
+                            <option value="Thuesday"></option>
+                            <option value="Wednesday"></option>
+                            <option value="Thurday"></option>
+                            <option value="Friday"></option>
+                            <option value="Saterday"></option>
+                            <option value="Sunday"></option>
+                        </datalist>
+                    </div>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </aside>
+            </main>
+        </section>
+
     </>
   )
 }
